f1a4ec1f8497fd197fb639041e0415d57cb06f0e
arika <arika.khor@gmail.com>
2026-02-25 19:10:08 -0600

    add move script

---
+++ move.sh
@@ -0,0 +1,2 @@
+ros2 topic pub /ackermann_cmd ackermann_msgs/msg/AckermannDriveStamped "{header: {stamp: {sec: 0, nanosec: 0},
+     frame_id: 'base_link'}, drive: {steering_angle: 0.00, speed: 0.5, acceleration: 0.5}}"
