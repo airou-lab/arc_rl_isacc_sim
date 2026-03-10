f446ad6227e7a5a771a2009a2b11378a02f1a18a
arika <arika.khor@gmail.com>
2026-03-09 23:01:58 -0500

    remove a bunch of trash..

--- trash/test_sim_app.py
+++
@@ -1,9 +0,0 @@
-
-import os
-print("Starting script...")
-from isaacsim import SimulationApp
-print("Imported SimulationApp. Initializing...")
-simulation_app = SimulationApp({"headless": False})
-print("SimulationApp initialized successfully!")
-simulation_app.close()
-print("SimulationApp closed.")
